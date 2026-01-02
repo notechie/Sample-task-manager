ac5e6f77fcfd6c6c0aad8af09991994ed6fed388
Bhuban Mahapatra <100858314+notechie@users.noreply.github.com>
2026-01-02 20:09:52 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,12 +3,26 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  const hashed = await bcrypt.hash(password, 10);
+export const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-  const user = await User.create({ name, email, password: hashed });
-  res.status(201).json(user);
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashed });
+    
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
@@ -26,3 +40,4 @@ export const login = async (req, res) => {
 
   res.json({ token });
 };
+
